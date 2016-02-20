63e24439860a7b008f598f940b188f9b6056a74f
Peter Mottram (SysPete) <peter@sysnix.com>
2016-02-20 14:44:59 +0100

    add Makefile.PL

--- lib/Icecat/Schema.pm
+++ lib/Icecat/Schema.pm
@@ -1,6 +1,18 @@
 use utf8;
 package Icecat::Schema;
 
+=head1 NAME
+
+Icecat::Schema - DBIx::Class schema for icecat.biz data
+
+=head1 VERSION
+
+0.001
+
+=cut
+
+our $VERSION = 0.001;
+
 use strict;
 use warnings;
 
